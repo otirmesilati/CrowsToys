79059cc23b0af194bea519779782765e189eae72
Otir <45825931+otirmesilati@users.noreply.github.com>
2024-05-25 08:34:35 +0300

    noice! Maling!

--- Hello_Windows.c
+++ Hello_Windows.c
@@ -7,7 +7,7 @@ int main()
         NULL,
         L"My first message box!",
         L"Hello World!",
-        MB_YESNOCANCEL
+        MB_YESNOCANCEL | MB_ICONEXCLAMATION
     );
     return EXIT_SUCCESS;
 }
